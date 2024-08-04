fa4b4fbc2dd9f2aa332cc8bb1e4560204d831094
Eds2369 <65536358+Eds2369@users.noreply.github.com>
2024-08-04 16:55:37 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const emailInput = document.getElementById('email')
+
+const emailSubmitBtn = document.getElementById('submit-form')
+
+const emailAlert = document.getElementById('email-error')
+
+
+
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+emailSubmitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const email = emailInput.value.trim()
+
+    if(validateEmail(email)) {
+        emailInput.value = ''
+        emailAlert.classList.remove('active')
+    } else {
+        emailAlert.classList.add('active')
+    }
+})
